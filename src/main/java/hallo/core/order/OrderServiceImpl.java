8f7797beb30557696f37c56d213cfc2d928684f6
shsh0412 <0412corn@naver.com>
2023-04-06 17:39:29 +0900

    [add] 주문 할인 도메인 개발, 새로운 할인 정책
    * 새로운 할인 정책 개발
    * 관심사의 분리

--- src/main/java/hallo/core/order/OrderServiceImpl.java
+++ src/main/java/hallo/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hallo.core.order;
 
 import hallo.core.discount.DiscountPolicy;
 import hallo.core.discount.FixDiscountPolicy;
+import hallo.core.discount.RateDiscountPolicy;
 import hallo.core.member.Member;
 import hallo.core.member.MemberRepositoy;
 import hallo.core.member.MemoryMemberRepository;
@@ -9,7 +10,11 @@ import hallo.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements  OrderService{
 
     private final MemberRepositoy memberRepositoy = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    // 구현체의 의존성 삭제, 인터페이스에만 의존!!
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
