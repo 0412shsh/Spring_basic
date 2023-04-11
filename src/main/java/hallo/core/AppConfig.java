27442c2111786c11739af90b7d0b71d86226bbd5
shsh0412 <0412corn@naver.com>
2023-04-11 13:27:41 +0900

    [add] AppConfig 리팩터링

--- src/main/java/hallo/core/AppConfig.java
+++ src/main/java/hallo/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hallo.core;
 
+import hallo.core.discount.DiscountPolicy;
 import hallo.core.discount.FixDiscountPolicy;
+import hallo.core.member.MemberRepositoy;
 import hallo.core.member.MemberService;
 import hallo.core.member.MemberServiceImpl;
 import hallo.core.member.MemoryMemberRepository;
@@ -10,12 +12,19 @@ import hallo.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     } // 생성자 주입
 
-    public OrderService oderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private MemberRepositoy memberRepository() {
+        return new MemoryMemberRepository();
     }
 
+    public OrderService oderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
 }
 
