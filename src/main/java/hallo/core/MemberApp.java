c1be6fad866e5c723a7e9e6516f7a89f99652ef8
shsh0412 <0412corn@naver.com>
2023-04-12 14:45:48 +0900

    [add] 스프링으로 전환
    * DI 컨테이너 사용

--- src/main/java/hallo/core/MemberApp.java
+++ src/main/java/hallo/core/MemberApp.java
@@ -4,17 +4,24 @@ import hallo.core.member.Grade;
 import hallo.core.member.Member;
 import hallo.core.member.MemberService;
 import hallo.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
         // MemberService 인터페이스 받아오기
         // ==> 생성자로 Impl를 생성해서 MemberService안에는 MemberServiceImpl가 들어가 있음
         // MemberServiceImpl를 만들고 나는 MemoryMemberRepository를 사용할거야라고 주입 !
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+       // AppConfig appConfig = new AppConfig();
+       // MemberService memberService = appConfig.memberService();
 
         // 기존에는 MemberApp의 메인메서드에서 ServiceImpl을 직접 생성해주었음
         // MemberService memberService = new MemberServiceImpl();
+
+        // 스프링 사용 - AppConfig에 있는 환경설정 정보를 가지고 클래스 안에 있는 객체들을 가져와 관리해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // 메서드 이름과, 반환타입
+
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
--- src/main/java/hallo/core/OrderApp.java
+++ src/main/java/hallo/core/OrderApp.java
@@ -7,13 +7,20 @@ import hallo.core.member.MemberServiceImpl;
 import hallo.core.order.Order;
 import hallo.core.order.OrderService;
 import hallo.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.oderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.oderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService",OrderService.class);
+
 
         // MemberService memberService = new MemberServiceImpl();
         // OrderService orderService = new OrderServiceImpl(,);
